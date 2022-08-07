6845d90034e94497cf65473c46a81329c358f6cf
asus <kkw-11@naver.com>
2022-08-07 23:25:12 +0900

    feature: 상세보기

--- src/main/java/com/boardproject/myrest/controller/BoardController.java
+++ src/main/java/com/boardproject/myrest/controller/BoardController.java
@@ -5,10 +5,7 @@ import com.boardproject.myrest.repository.BoardRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -26,8 +23,14 @@ public class BoardController {
     }
 
     @GetMapping("/form")
-    public String form(Model model){
-        model.addAttribute("board",new Board());
+    public String form(Model model, @RequestParam(required = false) Long id){
+        if(id == null){
+            model.addAttribute("board",new Board());
+        }
+        else{
+            Board board = boardRepository.findById(id).orElse(null);
+            model.addAttribute("board",board);
+        }
         return "board/form";
     }
 
