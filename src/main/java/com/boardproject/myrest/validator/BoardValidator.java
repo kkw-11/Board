f56a1a25732d789ac72426becdfacac77e40969b
kiwon <kiwon.kim@seeroo.co.kr>
2022-08-09 13:11:13 +0900

    chore: gitignore add

--- src/main/java/com/boardproject/myrest/validator/BoardValidator.java
+++ src/main/java/com/boardproject/myrest/validator/BoardValidator.java
@@ -6,6 +6,7 @@ import org.springframework.util.StringUtils;
 import org.springframework.validation.Errors;
 import org.springframework.validation.Validator;
 
+
 @Component
 public class BoardValidator implements Validator {
     @Override
