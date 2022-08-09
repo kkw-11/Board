20b776701c5ce3df49f449a481c127f4b699af8f
kiwon <kiwon.kim@seeroo.co.kr>
2022-08-09 10:15:50 +0900

    feature: pagination

--- src/main/java/com/boardproject/myrest/controller/BoardController.java
+++ src/main/java/com/boardproject/myrest/controller/BoardController.java
@@ -6,6 +6,8 @@ import com.boardproject.myrest.validator.BoardValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -22,9 +24,16 @@ public class BoardController {
     @Autowired
     private BoardValidator boardValidator;
 
+    ///list?page=0&size=1
     @GetMapping("list")
-    public String list(Model model){
-        Page<Board> boards = boardRepository.findAll(PageRequest.of(0, 20));
+    public String list(Model model, @PageableDefault(size = 2) Pageable pageable){
+        Page<Board> boards = boardRepository.findAll(pageable);
+        int startPage = Math.max(1, boards.getPageable().getPageNumber() - 4);
+        int endPage = Math.min(boards.getTotalPages(), boards.getPageable().getPageNumber() + 4);
+
+
+        model.addAttribute("startPage",startPage);
+        model.addAttribute("endPage",endPage);
         model.addAttribute("boards",boards);
         return "board/list";
     }
